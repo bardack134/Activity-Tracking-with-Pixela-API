5f218092d1467bd23ca4194e34b0e8d6960847f0
Ricardo Antonio Gomez Villalobos <bardack134@gmail.com>
2024-04-03 18:12:09 +0900

    enviando informacion a nuestra grafica

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import requests
 from constants import *
+from datetime import date
 
 
-
-#TODO:usaremos el metodo post para crear una cuenta
+#TODO:USAREMOS EL METODO POST PARA CREAR UNA CUENTA
 pixela_api="https://pixe.la/v1/users"
 
 
@@ -25,24 +25,60 @@ parameters={
 # print(response.text)
 
 
-#vamos a crear un nuevo graph para nuestro usuario
+#TODO:VAMOS A CREAR UN NUEVO GRAPH PARA NUESTRO USUARIO
+# graph_headers={
+#     'X-USER-TOKEN':TOKEN 
+# }
+
+
+#informacion encontrada en la documentacion de la api
+# graph_parameters={
+#     "id":GRAPH_ID,
+#     "name":"Japanese Graph",
+#     "unit":"Hours",
+#     "type":"float",
+#     "color":"momiji",
+#     "timezone":"Asia/Tokyo",
+# }
+
+
+# graph_end_point=f'https://pixe.la/v1/users/{USERNAME}/graphs'
+
+
+#post request para crear nuestra grafica
+# response=requests.post(graph_end_point, json=graph_parameters, headers=graph_headers)
+
+
+# miramos la respuesta del servidor, para saber si fue exitosa o no nuestra peticion
+# print(response.text)
+
+
+#NOTE: https://docs.pixe.la/entry/post-graph (https://pixe.la/v1/users/bardack134/graphs/graph134.html)
+#to check our graph, more information in api documentaqtion
+
+
+#TODO: ENVIANDO INFORMACION A NUESTRA GRAFICA
+graph_end_point=f'https://pixe.la/v1/users/{USERNAME}/graphs/{GRAPH_ID}'
+
+
 graph_headers={
     'X-USER-TOKEN':TOKEN 
 }
 
 
-#informacion encontrada en la documentacion de la api
+# Obtenemos la fecha de hoy y la convertimos a una cadena
+today = str(date.today())
+
+
+# Reemplazamos los guiones en la fecha con nada (los eliminamos)
+today = today.replace('-', '')
+
+
 graph_parameters={
-    "id":"graph134",
-    "name":"Japanese Graph",
-    "unit":"Hours",
-    "type":"float",
-    "color":"momiji",
-    "timezone":"Asia/Tokyo",
-}
-
-
-graph_end_point=f'https://pixe.la/v1/users/{USERNAME}/graphs'
+    "date":today,
+    "quantity":"2.25",
+    "optionalData":"{\"key\":\"value\"}"
+    }
 
 
 #post request para crear nuestra grafica
@@ -51,12 +87,3 @@ response=requests.post(graph_end_point, json=graph_parameters, headers=graph_hea
 
 # miramos la respuesta del servidor, para saber si fue exitosa o no nuestra peticion
 print(response.text)
-
-
-#NOTE: https://docs.pixe.la/entry/post-graph
-#to check our graph, see api documentaqtion
-
-
-#Enviando informacion a nuestra grafica
-
-
